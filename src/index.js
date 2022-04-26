83b5aa6cda591cce72e880d5a36a5c8c2e31498f
lenaStsk <staskevichalena@gmail.com>
2022-04-26 22:10:31 +0300

    feat: add transform-array task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
-function calculateHanoi( disksNumber) {
-  
-}
+function transform(arr) {
+    if (!Array.isArray(arr) || arr === undefined){
+      throw new Error('\'arr\' parameter must be an instance of the Array!');
+    }
+    if ( !(arr instanceof Array)){
+      throw new Error('\'arr\' parameter must be an instance of the Array!');
+    }
+    if (arr.length == 0) {
+      return [];
+    }
+    let newArr = arr.slice();
+    for (let i=0; i<newArr.length; i++) {
+      if (newArr[i] === '--double-next') {
+          if(i === newArr.length-1) {
+             newArr.splice(i,1);   
+          } else {
+            newArr.splice(i,1,newArr[i+1]);
+          }
+      }
+      if (newArr[i] === '--discard-next') {
+          if (newArr[i+2] == '--double-prev' || newArr[i+2] == '--discard-prev' ) {
+            newArr.splice(i,3);
+          } else {
+            newArr.splice(i,2);
+          }
+      }
+      if (newArr[i] === '--double-prev') {
+        if(i === 0) {
+          newArr.splice(i,1);
+        } else {
+            newArr.splice(i,1,arr[i-1]);
+        }
+      }
+      if (newArr[i] === '--discard-prev') {
+        if(i === 0) {
+          newArr.splice(i,1);
+        } else {
+            newArr.splice(i-1,2);
+        }
+      }
+    } 
+    return newArr;
+  }
 
-let disksNumber = 9;
-console.log(calculateHanoi(disksNumber));
+
+let arr =[1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5];
+console.log(transform(arr));
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr) || arr === undefined){
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  if ( !(arr instanceof Array)){
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  if (arr.length == 0) {
+    return [];
+  }
+  let newArr = arr.slice();
+  for (let i=0; i<newArr.length; i++) {
+    if (newArr[i] === '--double-next') {
+        if(i === newArr.length-1) {
+           newArr.splice(i,1);   
+        } else {
+          newArr.splice(i,1,newArr[i+1]);
+        }
+    }
+    if (newArr[i] === '--discard-next') {
+        if (newArr[i+2] == '--double-prev' || newArr[i+2] == '--discard-prev' ) {
+          newArr.splice(i,3);
+        } else {
+          newArr.splice(i,2);
+        }
+    }
+    if (newArr[i] === '--double-prev') {
+      if(i === 0) {
+        newArr.splice(i,1);
+      } else {
+          newArr.splice(i,1,arr[i-1]);
+      }
+    }
+    if (newArr[i] === '--discard-prev') {
+      if(i === 0) {
+        newArr.splice(i,1);
+      } else {
+          newArr.splice(i-1,2);
+      }
+    }
+  } 
+  return newArr;
 }
 
 module.exports = {
