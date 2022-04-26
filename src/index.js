487c7d1c85dcb561aeeda19f0c9155b7c4fd2a6d
lenaStsk <staskevichalena@gmail.com>
2022-04-26 19:33:10 +0300

    feat: add hanoi-tower task

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-function getSeason(date) {
-    let season = '';
-    let month;
-    if (Object.getOwnPropertyNames(date).length == 0 && date != null) {
-      if (date instanceof Date) {
-        month = date.getMonth();
-        if (month > 1 && month < 5) {
-          return 'spring';
-        }
-        else if (month > 4 && month < 8) {
-          return 'summer';
-        }
-        else if (month > 7 && month < 11) {
-          return 'autumn';
-        } else {
-          return 'winter';
-        }
-      } else {
-        throw new Error("Invalid date!");
-      }
-    } else {
-      throw new Error("Invalid date!");
-    }
-    
-  }
+function calculateHanoi( disksNumber) {
+  
+}
 
-  let date = new Date(1456, 0, 2, 1, 50, 9, 238);
-  console.log(getSeason());
+let disksNumber = 9;
+console.log(calculateHanoi(disksNumber));
   
