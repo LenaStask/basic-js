53be594d7390758461b76417b5faed3bb097de74
lenaStsk <staskevichalena@gmail.com>
2022-04-27 17:23:37 +0300

    feat: add mine-sweeper task

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
-function getMatrixElementsSum(matrix) {
+function minesweeper(matrix) {
+  let newMatrix=[];
   let sum=0;
-  for (let i=0; i<matrix.length; i++){
+  for (let i=0; i<matrix.length; i++) {
+    newMatrix[i]=[];
     for (let j=0; j<matrix[i].length; j++) {
-        if (i===0) {
-          sum+=matrix[i][j];
-        } else{
-          if ( matrix[i-1][j] != 0) {
-            sum+=matrix[i][j];
-          }
-          
-        }
+      if (i>0 && j>0 && matrix[i-1][j-1]) {
+        sum+=1;
+      }
+      if (i>0 && matrix[i-1][j]) {
+          sum+=1;
+      }
+      if (i>0 && j<matrix[i].length-1 && matrix[i-1][j+1]) {
+        sum+=1;
+      }
+      if (j<matrix[i].length-1 && matrix[i][j+1]) {
+        sum+=1;
+      }
+      if (i<matrix.length-1 && j<matrix[i].length-1 && matrix[i+1][j+1]) {
+        sum+=1;
+      }
+      if (i<matrix.length-1 && j>0 && matrix[i+1][j]) {
+        sum+=1;
+      }
+      if (i<matrix.length-1 && j>0 && matrix[i+1][j-1]) {
+        sum+=1;
+      }
+      if (j>0 && matrix[i][j-1]) {
+        sum+=1;
+      }
+      newMatrix[i][j]=sum;
+      sum = 0;
     }
   }
-  return sum;
+  return newMatrix;
 }
-
-console.log(getMatrixElementsSum([
-  [0, 1, 1, 2],
-  [0, 5, 0, 0],
-  [2, 0, 3, 3],
+console.log(minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
 ]));
   
