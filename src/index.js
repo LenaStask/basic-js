bd3ffc529f7a0784d797120d61664742c4663bf5
lenaStsk <staskevichalena@gmail.com>
2022-04-27 17:43:40 +0300

    feat: add sum-digits task

--- src/index.js
+++ src/index.js
@@ -1,42 +1,14 @@
-function minesweeper(matrix) {
-  let newMatrix=[];
-  let sum=0;
-  for (let i=0; i<matrix.length; i++) {
-    newMatrix[i]=[];
-    for (let j=0; j<matrix[i].length; j++) {
-      if (i>0 && j>0 && matrix[i-1][j-1]) {
-        sum+=1;
-      }
-      if (i>0 && matrix[i-1][j]) {
-          sum+=1;
-      }
-      if (i>0 && j<matrix[i].length-1 && matrix[i-1][j+1]) {
-        sum+=1;
-      }
-      if (j<matrix[i].length-1 && matrix[i][j+1]) {
-        sum+=1;
-      }
-      if (i<matrix.length-1 && j<matrix[i].length-1 && matrix[i+1][j+1]) {
-        sum+=1;
-      }
-      if (i<matrix.length-1 && j>0 && matrix[i+1][j]) {
-        sum+=1;
-      }
-      if (i<matrix.length-1 && j>0 && matrix[i+1][j-1]) {
-        sum+=1;
-      }
-      if (j>0 && matrix[i][j-1]) {
-        sum+=1;
-      }
-      newMatrix[i][j]=sum;
-      sum = 0;
+function getSumOfDigits(n) {
+  let arr = n.toString().split('');
+  let sum = 0;
+  while(arr.length > 1) {
+    sum=0;
+    for( let i=0; i<arr.length; i++) {
+      sum+=Number.parseInt(arr[i]);
     }
+    arr = sum.toString().split('');
   }
-  return newMatrix;
+  return arr;
 }
-console.log(minesweeper([
-  [true, false, false],
-  [false, true, false],
-  [false, false, false],
-]));
+console.log(getSumOfDigits(91));
   
