803d9cf80640bda0714377e3142b83c390887e96
lenaStsk <staskevichalena@gmail.com>
2022-04-27 23:47:33 +0300

    feat:add file-names task

--- src/file-names.js
+++ src/file-names.js
@@ -15,8 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(names) {
-  
+ function renameFiles(names) {
+  let k=1;
+  for (let i=0; i<names.length; i++) {
+    for (let j=i+1; j<names.length; j++) {
+      if (names[i]===names[j]) {
+        if (names[j].includes('(') && names[j].includes(')')) {
+          k = names[j].substring(names[j].length-2,names[j].length-1);
+          k = Number.parseInt(k);
+          names[j]=names[j]+`(${k})`;
+        } else {
+          names[j]=names[j]+`(${k})`;
+          k++;
+        }
+      }
+    }
+  }
+  return names;
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-function isMAC48Address(n) {
-  n = n.replace(/[0-9]/g,0);
-  n = n.replace(/[A-F]/g,0);
-  if( n == '00-00-00-00-00-00') {
-    return true;
-  } else {
-    return false;
+function renameFiles(names) {
+  let k=1;
+  for (let i=0; i<names.length; i++) {
+    for (let j=i+1; j<names.length; j++) {
+      if (names[i]===names[j]) {
+        if (names[j].includes('(') && names[j].includes(')')) {
+          k = names[j].substring(names[j].length-2,names[j].length-1);
+          k = Number.parseInt(k);
+          names[j]=names[j]+`(${k})`;
+        } else {
+          names[j]=names[j]+`(${k})`;
+          k++;
+        }
+      }
+    }
   }
+  return names;
 }
-console.log(isMAC48Address('G0-00-00-00-00-00'));
+console.log(renameFiles([]));
   
